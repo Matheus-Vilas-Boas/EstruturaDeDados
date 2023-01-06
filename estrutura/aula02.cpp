1eb8bd2dbde18d35cdbf31dec5f8f7fecea81430
MTH-VB <mthvboassiqueira@gmail.com>
2023-01-06 16:23:08 -0300

    02

--- estrutura/aula02.cpp
+++ estrutura/aula02.cpp
@@ -5,8 +5,27 @@ using namespace std;
 
 int main()
 {
-    
+    int inteiro;
+    inteiro = 35;
 
+    cout << inteiro << endl;
+
+    float real;
+    real = 3.6;
+    cout << real << endl;
+
+    bool boleano;
+    boleano = true;
+    cout << boleano << endl;
+
+    string text;
+    text = "ABACATE";
+    cout << text << endl;
+
+    char unit;
+    unit = 'b';
+    cout << unit << endl;
+    
     return 0;
 }
 
