512ce2b4aad6f1e3df57e8f2df040c09cc9f7595
MTH-VB <mthvboassiqueira@gmail.com>
2023-01-12 12:20:24 -0300

    Aula06

---
+++ estrutura/aula06.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+        int valor1;
+        int valor2;
+        int resu;
+    do
+    {
+        cout << "valor 1"  << endl;
+        cin >> valor1;
+
+        cout << "valor 2"  << endl;
+        cin >> valor2;
+        
+    } while (valor1 && valor2 < 0);
+
+        resu = (valor1 + valor2)/2;
+      if (resu >= 10)
+        {
+            cout << "valor = " << resu  << " Aprovado"<< endl; 
+        }
+        else if (resu < 10)
+        {
+            cout << "valor = " << resu  << " Reprovado"<< endl; 
+        }
+}
