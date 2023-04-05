7709ac7c0c110262e927b735f5778fbbaba454d3
mthvboas <mthvboassiqueira@gmail.com>
2023-04-05 14:48:24 -0300

    Inatel Aulas

--- InatelAulas/1117.cpp
+++ InatelAulas/1117.cpp
@@ -5,19 +5,27 @@ using namespace std;
 int main()
 {
     cout << fixed << setprecision(2);
-    int nota1, nota2;
-    int media;
-    cin >> nota1 >> nota2;
-
-    while (nota1 && nota2 < 0 || nota1 && nota2 > 10)
+    float x, y, z;
+    y = 0;
+    z = 0;
+    while(true)
     {
-        cout << "nota invalida";
-        cin >> nota1 >> nota2;
+        cin >> x;
+        if(x < 0 || x > 10)
+        {
+            cout << "nota invalida\n";
+        }
+        else if(z == 0)
+        {
+            y = x;
+            z = 1;
+        }
+        else
+        {
+            cout << "media " << (x+y)/2 ;
+            break;
+        }
     }
-    media = (nota1 + nota2)/2;
-    cout << media << endl;
-
-
-    
     return 0;
+
 }
