6ff0b7eefa2a7a9966678932b779c035ea6511ef
mthvboas <mthvboassiqueira@gmail.com>
2023-05-29 14:02:13 -0300

    Exercicios

--- Inatel/exemplos/arquivos.cpp
+++ Inatel/exemplos/arquivos.cpp
@@ -7,6 +7,21 @@ int main()
 {
     char nomearq[100];
     ofstream arq; //handle para escrever arquivos
-    strcpy(nomearq, "arquivos.txt")
+    char nomeFilme[100];
+    int ano;
+    strcpy(nomearq, "arquivos.txt");
+
+    //-------------INPUT----------------
+
+    cin.get(nomeFilme, 100);
+    cin >> ano;
+    //Abrindo arquivo
+    arq.open(nomearq, ofstream::out);
+
+    //Gravando dados
+    arq << nomeFilme << " " << ano << endl;
+    arq << ano << endl;
+    //fechando arquivo
+    arq.close();
     return 0;
 }
