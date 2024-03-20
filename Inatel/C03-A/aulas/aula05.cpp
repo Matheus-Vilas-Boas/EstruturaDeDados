197f8fb70e32ec7c80530827d9c4190807ce8006
mthvb <mthvboassiqueira@gmail.com>
2024-03-20 18:30:32 -0300

    Insertion sort

---
+++ Inatel/C03-A/aulas/aula05.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void insertion_sort(int vetor[], int tam)
+{
+    int i, j, aux;
+    for (j = 1; i < tam; j++) // 1 + 3(n) + 3(n-1)
+    {
+        aux = vetor[j]; // 3(n-1)
+
+        for (i = j-1; i >= 0 ; i--) //3(n-1) + 2(n-1) ((n/2) + 1) + 3 (n-1) (n/2)
+        {
+            if ( vetor[i] > aux) //4(n-1)(n/2)
+            {
+                vetor[i + 1] = vetor[i]; // 5(n-1)(n/2)
+            }
+            else
+            {
+                break;
+            }
+        } 
+        vetor[i+1] = aux;
+    } //O(n^2)
+}
+
+int main()
+{
+    int n = 7;
+    int vetor[n] = {7, 5, 2, 4, 1, 3, 6};
+
+    insertion_sort(vetor, n);
+    return 0;
+}
